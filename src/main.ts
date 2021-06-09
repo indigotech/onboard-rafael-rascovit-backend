0b89c6cbadd71e943cc9167f1ae34a86210e82cd
rafael.rascovit <rafaelrascovit@gmail.com>
2021-06-09 10:12:41 -0300

    Database setup

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,33 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer } from 'apollo-server';
+import resolvers from './resolvers';
+import typeDefs from './type-defs';
+import 'reflect-metadata';
+import { createConnection, Connection } from 'typeorm';
+import { User } from './entity/user';
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
+createConnection({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'rascotile',
+  password: '123123',
+  database: 'databoard_db',
+  entities: [User],
+  synchronize: true,
+  logging: false,
+})
+  .then((connection) => {
+    let user = new User();
+    user.firstName = 'Rafael';
+    user.lastName = 'Rascovit';
+    user.isActive = true;
 
-const resolvers = {
-  Query: {
-    hello: () => 'Hello, world!',
-  },
-};
+    return connection.manager.save(user).then((user) => {
+      console.log('User has been saved', user.id);
+    });
+  })
+  .catch((error) => console.log(error));
 
 const server = new ApolloServer({ resolvers, typeDefs });
 
-server.listen().then(({ url }) => console.log(`Running server at ${url}. `));
+server.listen().then(({ url }) => console.log(`Running server at ${url}.`));
