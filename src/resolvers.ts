6df838fb8c9f457362c35e9711f05eb08b1f5134
rafael.rascovit <rafaelrascovit@gmail.com>
2021-06-14 19:31:22 -0300

    Mutation test

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -4,6 +4,24 @@ import * as bcrypt from 'bcrypt';
 
 const saltRounds = 10;
 
+function isFutureDate(birthDate) {
+  let now = new Date();
+  let month = (now.getMonth() + 1).toString();
+  let newMonth = month.length == 1 ? '0' + month : month;
+  let day = now.getDate().toString();
+  let newDay = day.length == 1 ? '0' + day : day;
+  let year = now.getFullYear().toString();
+
+  let date = year + '/' + newMonth + '/' + newDay;
+  let newBirth = birthDate.split('/')[2] + '/' + birthDate.split('/')[1] + '/' + birthDate.split('/')[0];
+
+  if(Date.parse(newBirth) > Date.parse(date)){
+    return true;
+  } else {
+    return false;
+  }
+}
+
 export default {
   Query: {
     hello: () => 'Hello, world!',
@@ -25,13 +43,24 @@ export default {
   },
   Mutation: {
     createUser: async (_source, args) => {
+      var dtRegex =
+        /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+      const emailRegex =
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      const passRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,}$/;
       const email = await getRepository(User).findOne({
         email: args.email,
       });
-      if (args.password.length < 7) {
-        return;
+      if (!passRegex.test(args.password)) {
+        throw 'Senha precisa conter 7 dígitos com pelo menos uma letra e um número';
       } else if (email) {
-        return;
+        throw 'E-mail ja cadastrado';
+      } else if (!emailRegex.test(args.email)) {
+        throw 'E-mail inválido';
+      } else if (!dtRegex.test(args.birthDate)) {
+        throw 'A data de nascimento deve estar no formato dd/mm/yyyy';
+      } else if (isFutureDate(args.birthDate)) {
+        throw 'A data de nascimento está no futuro';
       }
       const user = new User();
       user.name = args.name;
--- test/index.ts
+++ test/index.ts
@@ -1,29 +1,13 @@
-import { ApolloServer } from 'apollo-server';
-import * as assert from 'assert';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: 'test.env' });
 import * as request from 'supertest';
-import { createConnection } from 'typeorm';
+import { expect } from 'chai';
+import { runServer, closeServer } from '../src/main';
+import { getRepository } from 'typeorm';
 import { User } from '../src/entity/user';
-import resolvers from '../src/resolvers';
-import typeDefs from '../src/type-defs';
 
 before(async () => {
-  createConnection({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'rascotile',
-    password: '123123',
-    database: 'databoard_db',
-    entities: [User],
-    synchronize: true,
-    logging: false,
-  })
-    .then(() => {})
-    .catch((error) => console.log(error));
-
-  const server = new ApolloServer({ resolvers, typeDefs });
-
-  await server.listen().then(({ url }) => console.log(`Running server at ${url}.`));
+  await runServer();
 });
 
 describe('Hello world test', () => {
@@ -35,6 +19,142 @@ describe('Hello world test', () => {
             hello
         }`,
       });
-      console.log(response.body.data);
+
+    expect(response.statusCode).to.equal(200);
+    expect(response.body.data.hello).to.be.eq('Hello, world!');
   });
+});
+
+describe('Create user test', () => {
+  it('should create an user in the test database and return his details', async () => {
+    const response = await request('localhost:4000')
+      .post('/')
+      .send({
+        query: `mutation{
+            createUser(
+                name:"Teste",
+                email:"test1@test.com",
+                password:"test123",
+                birthDate:"17/09/1991"
+            ){
+                id
+                name
+                email
+                password
+                birthDate
+            }
+        }`,
+      });
+
+    expect(response.statusCode).to.be.eq(200);
+    expect(response.body.data.createUser.id).to.be.a('number');
+    expect(response.body.data.createUser.name).to.be.eq('Teste');
+    expect(response.body.data.createUser.email).to.be.eq('test1@test.com');
+    expect(response.body.data.createUser.birthDate).to.be.eq('17/09/1991');
+  });
+});
+
+describe('Create user test - Error case', () => {
+  it('should fail to create user due to invalid email', async () => {
+    const response = await request('localhost:4000')
+      .post('/')
+      .send({
+        query: `mutation{
+            createUser(
+                name:"Teste",
+                email:"test@",
+                password:"test123",
+                birthDate:"17/09/1991"
+            ){
+                id
+                name
+                email
+                password
+                birthDate
+            }
+        }`,
+      });
+    expect(response.body.errors[0].message).to.be.eq('Unexpected error value: "E-mail inválido"');
+  });
+});
+
+describe('Create user test - Error case', () => {
+  it('should fail to create user due to short password', async () => {
+    const response = await request('localhost:4000')
+      .post('/')
+      .send({
+        query: `mutation{
+            createUser(
+                name:"Teste",
+                email:"test2@test.com",
+                password:"test1",
+                birthDate:"17/09/1991"
+            ){
+                id
+                name
+                email
+                password
+                birthDate
+            }
+        }`,
+      });
+    expect(response.body.errors[0].message).to.be.eq(
+      'Unexpected error value: "Senha precisa conter 7 dígitos com pelo menos uma letra e um número"',
+    );
+  });
+});
+
+describe('Create user test - Error case', () => {
+  it('should fail to create user due to without number password', async () => {
+    const response = await request('localhost:4000')
+      .post('/')
+      .send({
+        query: `mutation{
+            createUser(
+                name:"Teste",
+                email:"test3@test.com",
+                password:"testtest",
+                birthDate:"17/09/1991"
+            ){
+                id
+                name
+                email
+                password
+                birthDate
+            }
+        }`,
+      });
+    expect(response.body.errors[0].message).to.be.eq(
+      'Unexpected error value: "Senha precisa conter 7 dígitos com pelo menos uma letra e um número"',
+    );
+  });
+});
+
+describe('Create user test - Error case', () => {
+  it('should fail to create user due to future date', async () => {
+    const response = await request('localhost:4000')
+      .post('/')
+      .send({
+        query: `mutation{
+            createUser(
+                name:"Teste",
+                email:"test4@test.com",
+                password:"test123",
+                birthDate:"11/07/2022"
+            ){
+                id
+                name
+                email
+                password
+                birthDate
+            }
+        }`,
+      });
+    expect(response.body.errors[0].message).to.be.eq('Unexpected error value: "A data de nascimento está no futuro"');
+  });
+});
+
+after(async () => {
+  await getRepository(User).query('DROP TABLE "user"');
+  await closeServer();
 });
